90f7567cfbf2bf858c0e7d8f178b13dc5b795135
Jarred Bishop <jarredbishop@gmail.com>
2015-02-26 19:08:36 +0900

    fix for homepage?

--- client/scripts/home.js
+++ client/scripts/home.js
@@ -2,6 +2,18 @@ Meteor.startup(function(){
     Mapbox.load();
 });
 
+Template.Home.helpers({
+  items: function(){
+    var items = Items.find().fetch();
+    _.each(items, function(item){
+      var category = Categories.findOne(item.category)
+      item.category_name = category.name
+    });
+    return items;
+  },
+  categories: Categories.find()
+});
+
 Template.Home.rendered = function () {
   this.autorun(function () {
     if (Mapbox.loaded()) {
